426baf191767132490eab0e4f1bc7462fff6231d
Vlad-Stefan Harbuz <vlad@vlad.website>
2026-06-02 20:32:01 +0100

    add comment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,10 @@ export default defineNuxtConfig({
     '@nuxt/fonts',
   ],
   devtools: { enabled: true },
+  features: {
+    // The below line does not affect the reproduction.
+    // inlineStyles: true,
+  },
   css: ['~/assets/css/main.css'],
   future: { compatibilityVersion: 4 },
   compatibilityDate: 'latest',
